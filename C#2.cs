b7ff4de28df10fe6f05aa7a3b753a47d956292be
Sakethram Nandiraju <Sakethram.n@tezo.com>
2026-06-29 11:43:42 +0530

    This is upto date now

---
+++ C#2.cs
@@ -0,0 +1,18 @@
+int a = 10;
+int b = 11;
+if  (a == b)
+{
+    return true;
+}
+else if (b > a)
+{
+    Console.WriteLine("return b greater");
+}
+else if(a > b)
+{
+    Console.WriteLine("return a greater");
+}
+else
+{
+    Console.WriteLine("This is done");
+}
