3bb6ac8d7632d1be99ebfd2ea3f83f3527e465ac
Sakethram Nandiraju <Sakethram.n@tezo.com>
2026-06-29 11:48:33 +0530

    This local branch

--- C#2.cs
+++ C#2.cs
@@ -1,3 +1,22 @@
 int a = 10;
 int b = 11;
-Console.WriteLine(a + b);
+if (a == b)
+{
+    return true;
+}
+else if (a < b)
+{
+    Console.WriteLine("Return b true");
+}
+else if  ((a == 10) && (b == 11))
+{
+    Conosle.WriteLine("the values are these");
+}
+else if (a > b)
+{
+    Console.WriteLine("A is greater than b");
+}
+else
+{
+    Console,WriteLine("All values don't match");
+}
